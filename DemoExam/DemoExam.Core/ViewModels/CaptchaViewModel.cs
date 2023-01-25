293870b2c91eab8b1b3e05b0fcba00e4f70c5d94
Artem Levin <leoartemlev@yandex.ru>
2023-01-25 22:44:11 +0500

    added view model for captcha

---
+++ DemoExam/DemoExam.Core/ViewModels/CaptchaViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+using DemoExam.Core.Utils;
+using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+
+namespace DemoExam.Core.ViewModels;
+
+public class CaptchaViewModel : MvxViewModel
+{
+    private string _captchaText;
+    public string CaptchaText
+    {
+        get => _captchaText;
+        set => SetProperty(ref _captchaText, value);
+    }
+
+    private string _userInput;
+    public string UserInput
+    {
+        get => _userInput;
+        set => SetProperty(ref _userInput, value);
+    }
+
+    private MvxCommand? _verifyCommand;
+    public ICommand VerifyCommand => _verifyCommand ??= new MvxCommand(() =>
+    {
+        if (UserInput == CaptchaText)
+        {
+            // TODO on success captcha pass
+        }
+    });
+
+    public CaptchaViewModel()
+    {
+        CaptchaText = CaptchaTextGenerator.GenerateCaptchaText();
+    }
+}
