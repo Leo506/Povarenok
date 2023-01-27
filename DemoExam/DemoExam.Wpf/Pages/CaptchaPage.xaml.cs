80ed146137c3458806663e97ad65180c8621e1bd
Artem Levin <leoartemlev@yandex.ru>
2023-01-27 22:01:19 +0500

    added UI for captcha

---
+++ DemoExam/DemoExam.Wpf/Pages/CaptchaPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace DemoExam.Wpf.Pages;
+
+public partial class CaptchaPage : MvxWpfView
+{
+    public CaptchaPage()
+    {
+        InitializeComponent();
+    }
+}
