7c9f335f8a36f369c3bddb60d59bfc6e672e7d5f
Artem Levin <leoartemlev@yandex.ru>
2023-01-31 22:29:28 +0500

    create ui for product lists

---
+++ DemoExam/DemoExam.Wpf/Converters/NumberToDiscountStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DemoExam.Wpf.Converters;
+
+public class NumberToDiscountStringConverter : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var discount = (byte)value;
+        return $"Discount {discount}%";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
