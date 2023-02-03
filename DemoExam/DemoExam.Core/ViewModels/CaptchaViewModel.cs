71d4ebe40894b5d8f460aeb9ba5e44d1a27aae77
Artem Levin <leoartemlev@yandex.ru>
2023-02-03 21:59:40 +0500

    refactor CaptchaViewModel

--- DemoExam/DemoExam.Core/ViewModels/CaptchaViewModel.cs
+++ DemoExam/DemoExam.Core/ViewModels/CaptchaViewModel.cs
@@ -8,29 +8,28 @@ namespace DemoExam.Core.ViewModels;
 
 public class CaptchaViewModel : MvxViewModel<Action<bool>>
 {
-    private string _captchaText;
     public string CaptchaText
     {
         get => _captchaText;
         set => SetProperty(ref _captchaText, value);
     }
 
-    private string _userInput;
     public string UserInput
     {
         get => _userInput;
         set => SetProperty(ref _userInput, value);
     }
 
-    private MvxCommand? _verifyCommand;
     public ICommand VerifyCommand => _verifyCommand ??= new MvxCommand(async () =>
     {
         await _navigationService.Close(this).ConfigureAwait(false);
         _callBack(UserInput == CaptchaText);
     });
 
+    private string _captchaText;
+    private string _userInput;
+    private MvxCommand? _verifyCommand;
     private Action<bool> _callBack;
-
     private readonly IMvxNavigationService _navigationService;
     
     public CaptchaViewModel(IMvxNavigationService navigationService)
