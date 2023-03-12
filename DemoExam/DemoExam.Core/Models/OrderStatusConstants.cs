8e9eade4ca5cb9a52551f3378c09a42a90f02b76
Artem Levin <leoartemlev@yandex.ru>
2023-03-12 23:55:49 +0500

    created separated project for Entities

--- DemoExam/DemoExam.Core/Models/OrderStatusConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DemoExam.Core.Models;
-
-public static class OrderStatusConstants
-{
-    public static string NewOrder = "Новый";
-
-    public static string Completed = "Завершен";
-}
---
+++ DemoExam/DemoExam.Domain/Entities/OrderStatusConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoExam.Core.Models;
+
+public static class OrderStatusConstants
+{
+    public const string NewOrder = "Новый";
+
+    public const string Completed = "Завершен";
+}
