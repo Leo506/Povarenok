68f41ac8446186d513aee3aca94850091e853801
Artem Levin <leoartemlev@yandex.ru>
2023-03-15 20:25:30 +0500

    generating GetCode for order

--- DemoExam/DemoExam.Core/ViewModels/OrderViewModel.cs
+++ DemoExam/DemoExam.Core/ViewModels/OrderViewModel.cs
@@ -45,12 +45,14 @@ public class OrderViewModel : MvxViewModel<User>
     public override void Prepare(User parameter)
     {
         _user = parameter;
+        var random = new Random();
         Order = new Order
         {
             ClientName = _user.FullName,
             OrderData = DateTime.Now,
             OrderDeliveryDate = DateTime.Now.AddDays(3),
-            OrderStatus = OrderStatusConstants.NewOrder
+            OrderStatus = OrderStatusConstants.NewOrder,
+            GetCode = random.Next(1, 1000)
         };
     }
 
