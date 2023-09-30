d086a5e15b2c5e5ac8a6138fcd165587513efd5f
Artem Levin <leoartemlev@yandex.ru>
2023-09-30 21:26:09 +0500

    start web app

---
+++ DemoExam/DemoExam.Web/Controllers/CatalogController.cs
@@ -0,0 +1,13 @@
+﻿using DemoExam.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoExam.Web.Controllers;
+
+public class CatalogController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View(new ListOfProductsViewModel());
+    }
+}
---
+++ DemoExam/DemoExam.Web/Models/ListOfProductsViewModel.cs
@@ -0,0 +1,76 @@
+﻿using DemoExam.Domain.Models;
+
+namespace DemoExam.Web.Models;
+
+public class ListOfProductsViewModel
+{
+    public List<Product> Products { get; set; } = new()
+    {
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        },
+        new Product()
+        {
+            ProductName = "Вилка",
+            ProductDescription = "gegegegegegegeg",
+            ProductPhoto = File.ReadAllBytes(@"D:\DemoExam\DemoExam\DatabaseFiller\Pictures\B736H6.jpg")
+        }
+    };
+}
