02a0d8ad8bb9db20b2086c9250b11f1c0fa87224
Artem Levin <leoartemlev@yandex.ru>
2023-11-20 23:27:37 +0500

    registration refactoring

--- DemoExam/DemoExam.Blazor/ViewModels/RegistrationViewModel.cs
+++ DemoExam/DemoExam.Blazor/ViewModels/RegistrationViewModel.cs
@@ -4,9 +4,9 @@ namespace DemoExam.Blazor.ViewModels;
 
 public class RegistrationViewModel
 {
-    [Required] public string UserName { get; set; } = default!;
-    [Required] public string UserSurname { get; set; } = default!;
-    [Required] public string UserPatronymic { get; set; } = default!;
-    [Required] public string Login { get; set; } = default!;
-    [Required] public string Password { get; set; } = default!;
+    [Required(ErrorMessage = "Поле обязательно")] public string UserName { get; set; } = default!;
+    [Required(ErrorMessage = "Поле обязательно")] public string UserSurname { get; set; } = default!;
+    [Required(ErrorMessage = "Поле обязательно")] public string UserPatronymic { get; set; } = default!;
+    [Required(ErrorMessage = "Поле обязательно")] public string Login { get; set; } = default!;
+    [Required(ErrorMessage = "Поле обязательно")] public string Password { get; set; } = default!;
 }
